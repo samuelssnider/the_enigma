0af376c15ab9f5d0288e914a7395052654724f43
marvin.bonds <marvinbonds@yahoo.com>
2017-07-08 16:17:08 -0600

    Current Progress,needs work

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,1 +1,41 @@
-derp
+require 'date'
+class Enigma
+
+  attr_reader :key, :key_array, :date, :converted_date
+
+  def initialize
+    @key = 41521
+    @key_array = []
+    @offset_array = []
+    @rotation_array = []
+    building_key
+    @date = Date.new(2017,7,8)
+    @converted_date
+    date_conversion
+
+
+  end
+
+  def building_key
+    #refactor/combine
+    key_string = @key.to_s
+    @key_array << key_string[0..1].to_i
+    @key_array << key_string[1..2].to_i
+    @key_array << key_string[2..3].to_i
+    @key_array << key_string[3..4].to_i
+  end
+
+  def date_conversion
+    @converted_date = @date.day.to_s.rjust(2,'0')
+    @converted_date += @date.month.to_s.rjust(2,'0')
+    @converted_date += @date.year.to_s[2..3]
+  end
+
+  def cdate_offset
+    offset_string = @converted_date.to_i
+  end
+
+
+
+
+end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,5 +1,42 @@
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/enigma'
+
 
 class Code_Test <  Minitest::Test
+
+  def test_instance_of_Enigma
+    e = Enigma.new
+    refute_nil e
+  end
+
+  def test_key_exists
+    e = Enigma.new
+    assert_equal e.key, 41521
+    refute_nil e.key
+  end
+
+  def test_key_array_is_right
+    e = Enigma.new
+    assert_equal e.key_array, [41, 15, 52, 21]
+  end
+
+  def test_enigma_initializes_date
+    e = Enigma.new
+    assert_equal e.date.year, 2017
+    assert_equal e.date.month,7
+    assert_equal e.date.day, 8
+  end
+
+  def test_date_conversion
+    e = Enigma.new
+    assert_equal e.converted_date, "080717"
+  end
+
+  def test_offset_value
+    e = Enigma.new
+    assert_equal e.offset_array, [4,0,8,9]
+  end
+
+end  
